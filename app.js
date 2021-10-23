7edf449b46097f7fe5aa868ef4f20ebc7a5f5816
Raka Maheka Auramuwahid <rakamaheka@gmail.com>
2021-10-23 18:19:56 +0700

    initial repo

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const router = require('./routes/index')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const mongoose = require('mongoose')
+
+app.use(cors())
+
+mongoose.connect('mongodb+srv://rakamaheka:rakamaheka22@weebflix.rwkno.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false,useCreateIndex:true
+}).then(()=>console.log('connected to database'))
+.catch(err => console.log(err))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(fileUpload({createParentPath:true}))
+
+app.use('/api',router)
+app.use('/',(req,res)=>{
+    res.send({
+        message : 'Welcome To Unofficial Otakudesu Rest Api',
+        createdBy : 'KaedeNoKi Team ♥️'
+    })
+})
+app.use('/api',(req,res) =>{
+    res.send({
+        message:'check our github for more info',
+        github :'https://github.com/Kaede-No-Ki/otakudesu-rest-api'
+    })
+})
+
+
+app.use('*',(req,res) =>{
+    res.json({
+        'status':'not found path',
+        message: 'read the docs here https://github.com/Kaede-No-Ki/otakudesu-rest-api'
+    })
+})
+app.listen(port, () => {
+    console.log('listening on port', port)
+})
---
+++ helpers/episodeHelper.js
@@ -0,0 +1,26 @@
+const { default: Axios } = require("axios");
+const cheerio = require("cheerio");
+const get = async (url) => {
+  try {
+    const response = await Axios.get(url);
+    const $ = cheerio.load(response.data);
+    let source1 = $.html().search('"file":');
+    let source2 = $.html().search("'file':");
+    console.log(source1);
+    console.log(source2);
+    
+    if (source1 !== -1) {
+      const end = $.html().indexOf('","');
+      return $.html().substring(source1 + 8, end);
+    } else if (source2 !== -1) {
+      const end = $.html().indexOf("','");
+      return $.html().substring(source2 + 8, end);
+    }
+    const _url = $('body > #mediaplayer > source').attr('src');
+    return _url || '-';
+  } catch (error) {
+    return "-";
+  }
+};
+
+module.exports = { get };
